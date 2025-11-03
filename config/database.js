6cc27f2d53a4feab1dd8100f4db5f17d4af5a47e
sprim0 <sherrellprimo@gmail.com>
2025-11-02 23:46:35 -0500

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://sherrellprimo_db_user:1GLQTaDPIc2ogRAB@cluster0.wajnrkv.mongodb.net/trips?appName=Cluster0', 
+    'dbName': 'trips'
+};
---
+++ public/main.js
@@ -0,0 +1,72 @@
+var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var trash = document.getElementsByClassName("fa-trash");
+var thumbDown = document.getElementsByClassName("fa-thumbs-down");
+
+// Array.from(thumbUp).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         fetch('messages', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg,
+//             'thumbUp':thumbUp
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const location = encodeURIComponent (this.closest('li').querySelector('.location').innerText)
+        const date = encodeURIComponent (this.parentNode.parentNode.childNodes[3].innerText)
+        fetch(`messages?date=${date}&location=${location}`, {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'location': location,
+            'date': date
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
+
+
+// Array.from(thumbDown).forEach(function(element) {
+//       element.addEventListener('click', function(){
+        
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         fetch('messages', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg,
+//             'thumbDown':thumbDown
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
